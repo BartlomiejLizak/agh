2369047876993ab3e28c31d3252484989b27ea5d
Bartłomiej Lizak <blizak@student.agh.edu.pl>
2025-11-09 10:15:02 +0100

    wykonano zadanie 1 i część 2 do kodu

--- schematy_blokowe/schematy_blokowe.cpp
+++ schematy_blokowe/schematy_blokowe.cpp
@@ -1,17 +1,34 @@
 ﻿#include <iostream>
 
 using namespace std;
-
-int ile_nog(int kura, int krowa, int swinia)
+// Funkcja do zadania 1 
+int ile_nog(int kura, int krowa, int swinia) //zbieramy informacje o ilosci zwierząt i mnożymy je przez ilość nóg.
 {
 	int a;
 	a = (kura * 2) + (krowa * 4) + (swinia * 4);
 	return a;
 }
 
+void czyprzestepny(int rok)
+{
+	if (rok % 4 == 0 && (rok % 100 != 0 || rok % 400 == 0)) //sprawdzamy czy rok jest podzielny przez 4 i czy nie jest podzielny przez 100 lub czy jest podzielny przez 400
+	{
+		cout << rok << " jest rokiem przestepnym";
+	}
+	else 
+	{	
+		cout << rok << " nie jest rokiem przestepnym";
+
+	}
+}
+
+
 
 int main() {
-	int ku, kr, sw;
+	// ZADANIE 1 ----------------------------
+	int ku, kr, sw; //tworzymy zmienne dla zwierząt
+
+	cout << "ZADANIE 1 ----------------------------" << endl;
 
 	cout << "Podaj liczbe kur: ";
 	cin >> ku;
@@ -21,5 +38,16 @@ int main() {
 	cin >> sw;
 
 	cout << ile_nog(ku, kr, sw);
+
+	// ZADANIE 2 ----------------------------
+
+	int wybor;
+
+	cout << "ZADANIE 2 ----------------------------" << endl;
+
+	cout << "Podaj zadanie ktore chcesz wykonac" << endl;
+
+	cin >> wybor;
+
 
 }
